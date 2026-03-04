f1d9893b7f69e7f2d8e889fa410e7f1949916a9d
nwadikeekene4-lab <nwadikeekene4@gmail.com>
2026-03-04 04:22:41 +0100

    Update models.js

--- firstclient-backend/models.js
+++ firstclient-backend/models.js
@@ -2,14 +2,21 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("./config");
 
 const Product = sequelize.define("Product", {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
   name: DataTypes.STRING,
   price: DataTypes.FLOAT,
   image: DataTypes.TEXT, 
-  rating: { type: DataTypes.JSON, defaultValue: { stars: 0, count: 0 } }
+  rating: {
+    type: DataTypes.JSON,
+    defaultValue: { stars: 0, count: 0 }
+  }
 }, {
   timestamps: true,
-  paranoid: true // ✅ This enables Soft Delete
+  paranoid: true // ✅ Enables Soft Delete: records aren't erased
 });
 
 module.exports = Product;
