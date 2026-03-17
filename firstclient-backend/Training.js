419502b82ac594f836198d29a8e5051450984929
nwadikeekene4-lab <nwadikeekene4@gmail.com>
2026-03-17 01:02:23 +0100

    Update Training.js

--- firstclient-backend/Training.js
+++ firstclient-backend/Training.js
@@ -1,1 +1,24 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./database"); // Use your existing DB connection
 
+const Training = sequelize.define("Training", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  order: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  }
+});
+
+module.exports = Training;
